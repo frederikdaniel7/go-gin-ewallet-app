82c076355ac0f86c0539932a13baf0afd6cccf5a
Frederick Daniel Joshua Hutabarat <frederikdaniel07@gmail.com>
2024-02-23 18:45:10 +0700

    feat : add graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"git.garena.com/sea-labs-id/bootcamp/batch-03/frederik-hutabarat/assignment-go-rest-api/internal/handler"
 	"git.garena.com/sea-labs-id/bootcamp/batch-03/frederik-hutabarat/assignment-go-rest-api/internal/repository"
@@ -33,7 +39,32 @@ func main() {
 		User:        userHandler,
 		Transaction: transactionHandler,
 	})
-	if err := router.Run(":8081"); err != nil {
-		log.Fatal(err)
+	// if err := router.Run(":8081"); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	srv := &http.Server{
+		Addr:    ":8081",
+		Handler: router,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	<-ctx.Done()
+	log.Println("timeout of 5 seconds.")
+
+	log.Println("Server exiting")
 }
